21c31162f1c5bb2e48f2fc6ce2159d880249e50b
ChickenKooCoo <gordan.danny07@gmail.com>
2025-04-28 14:59:05 -0500

    Initial Commit

---
+++ Friend Compass 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Friend Compass 2.0
+//
+//  Created by Daniel G. Ilchev on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Friend Compass 2.0/Friend_Compass_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Friend_Compass_2_0App.swift
+//  Friend Compass 2.0
+//
+//  Created by Daniel G. Ilchev on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Friend_Compass_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
